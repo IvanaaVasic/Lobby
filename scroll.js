8aed27c6486d8682e722bb47a05e19e9892f9c7b
IvanaaVasic <ivana.p.vasic@gmail.com>
2020-11-06 03:11:25 +0100

    c

--- scroll.js
+++ scroll.js
@@ -5,14 +5,14 @@ $(function() {
 
             if (eTop - $(window).scrollTop() <= 45) {
 
-                if (($('.bla2').offset().top + $('.bla2').outerHeight(true) - $(window).scrollTop() - $('.menu-left-bar').outerHeight(true) >= 50)) {
+                if (($('.food-section').offset().top + $('.food-section').outerHeight(true) - $(window).scrollTop() - $('.menu-left-bar').outerHeight(true) >= 50)) {
                     $('.menu-left-bar').css('position', 'fixed');
                     $('.menu-left-bar').css('top', '0px');
                     $('.menu-left-bar').css('margin-top', '');
                     $('.menu-left-bar').css('padding-top', '15px');
 
                 } else {
-                    var pos = $('.bla2').offset().top + $('.bla2').outerHeight(true) - $('.menu-left-bar').outerHeight(true) - 120;
+                    var pos = $('.food-section').offset().top + $('.food-section').outerHeight(true) - $('.menu-left-bar').outerHeight(true) - 120;
                     $('.menu-left-bar').css('position', 'absolute');
                     $('.menu-left-bar').css('top', pos + 'px');
                     $('.menu-left-bar').css('margin-top', '');
